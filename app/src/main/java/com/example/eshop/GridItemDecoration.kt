f092477e55bbdbc0be956f7f5bdb23a67b186d75
ellesmate <lbdkira@gmail.com>
2020-11-18 15:55:17 +0300

    Add animations to main fragment.

---
+++ app/src/main/java/com/example/eshop/GridItemDecoration.kt
@@ -0,0 +1,16 @@
+package com.example.eshop
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class GridItemDecoration(private val largePadding: Int, private val smallPadding: Int) : RecyclerView.ItemDecoration() {
+
+    override fun getItemOffsets(outRect: Rect, view: View,
+                                parent: RecyclerView, state: RecyclerView.State) {
+        outRect.left = smallPadding
+        outRect.right = smallPadding
+        outRect.top = largePadding
+        outRect.bottom = largePadding
+    }
+}
--- app/src/main/java/com/example/eshop/MainFragment.kt
+++ app/src/main/java/com/example/eshop/MainFragment.kt
@@ -1,10 +1,19 @@
 package com.example.eshop
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateDecelerateInterpolator
+import androidx.activity.OnBackPressedCallback
+import androidx.core.animation.doOnEnd
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.eshop.adapters.CategoryAdapter
 import com.example.eshop.adapters.ProductAdapter
 import com.example.eshop.databinding.FragmentMainBinding
@@ -13,19 +22,22 @@ import com.example.eshop.models.Product
 
 class MainFragment : Fragment() {
 
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var onBackPressedCallback: OnBackPressedCallback
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
 
-        val categories = listOf<Category>(
+        val categories = listOf(
             Category("acoustic", "https://e-shopdotnet.herokuapp.com/images/acoustic_category.jpg"),
             Category("electric", "https://e-shopdotnet.herokuapp.com/images/electric_category.jpg"),
             Category("bass", "https://e-shopdotnet.herokuapp.com/images/bass_category.jpg")
         )
-        val products = listOf<Product>(
+        val products = listOf(
             Product("Naranda-GAG110CNA", "Naranda GAG110CNA", "https://e-shopdotnet.herokuapp.com/images/guitar_1.jpg", 182.0),
             Product("MD SDG653", "MD SDG653", "https://e-shopdotnet.herokuapp.com/images/guitar_2.jpg", 454.54),
             Product("Cort Sunset NY", "Cort Sunset NY", "https://e-shopdotnet.herokuapp.com/images/guitar_3.jpg", 915.20)
@@ -35,11 +47,78 @@ class MainFragment : Fragment() {
         val productAdapter = ProductAdapter()
 
         binding.categoryRecyclerview.adapter = categoryAdapter
+        binding.viewAllText.setOnClickListener {
+            action(binding)
+        }
+
+        binding.productRecyclerview.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        binding.productRecyclerview.addItemDecoration(GridItemDecoration(12, 6))
         binding.productRecyclerview.adapter = productAdapter
 
         categoryAdapter.submitList(categories)
         productAdapter.submitList(products)
 
+        view?.findNavController()?.addOnDestinationChangedListener { controller, destination, arguments ->
+
+        }
+
+        onBackPressedCallback = object : OnBackPressedCallback(shown) {
+            override fun handleOnBackPressed() {
+                binding.productList.smoothScrollTo(0, 0);
+                action(binding)
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
+
+        binding.productList.setOnScrollChangeListener { v: NestedScrollView?, scrollX: Int, scrollY: Int, oldScrollX: Int, oldScrollY: Int ->
+//            println("Old: $oldScrollY, new: $scrollY")
+            if (shown && oldScrollY < scrollY)
+            {
+                action(binding)
+            }
+        }
+
+        defaultBottomPadding = binding.productList.paddingBottom
+        action(binding, 0)
+
         return binding.root
+    }
+
+    private fun onBackButtonPressed() : Boolean {
+        return false
+    }
+
+
+    private var shown = false
+    private val interpolator = AccelerateDecelerateInterpolator()
+    private var defaultBottomPadding = 0
+
+    private fun action(binding: FragmentMainBinding, duration: Long = 500)
+    {
+        shown = !shown
+        onBackPressedCallback.isEnabled = !shown
+        val animatorSet = AnimatorSet()
+        animatorSet.removeAllListeners()
+        animatorSet.end()
+        animatorSet.cancel()
+
+        val translateY = context?.resources?.getDimensionPixelSize(R.dimen.productListMarginTop)!!
+
+        if (!shown)
+            binding.productList.setPadding(0, 0, 0, defaultBottomPadding)
+
+        val animator = ObjectAnimator.ofFloat(binding.productList, "translationY", (if (shown) translateY else 0).toFloat())
+        animator.duration = duration
+        if (interpolator != null) {
+            animator.interpolator = interpolator
+        }
+
+        animatorSet.play(animator)
+        animator.start()
+        animator.doOnEnd {
+            if (shown)
+                binding.productList.setPadding(0, 0, 0, translateY + defaultBottomPadding)
+        }
     }
 }
